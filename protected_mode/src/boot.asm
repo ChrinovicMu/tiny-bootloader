0cda9ef36b4979d805c092a44af26751c891b7a4
ChrinovicMu <chrinovicmu@gmail.com>
2025-04-15 19:42:46 +0200

    "from beyond 512 bytes achieved"

--- protected_mode/src/boot.asm
+++ protected_mode/src/boot.asm
@@ -11,10 +11,28 @@ start:
     int 0x10 
     cli 
 
+    mov ah, 0x2 ;read sector 
+    mov al, 1   ;sector sto read 
+    mov ch, 0   ;cylinder idx 
+    mov dh, 0   ;head idx 
+    mov cl, 2   ;sector idx 
+    mov dl, [disk]; disk index 
+    mov bx, copy_target; target pointer 
+    int 0x13 
+
+    cli 
+
     lgdt [gdt_pointer]
+    
     mov eax, cr0 
     or  eax, 0x1 
     mov cr0, eax 
+    mov ax, DATA_SEG
+    mov ds, ax 
+    mov es, ax 
+    mov fs, ax 
+    mov gs, ax 
+    mov ss, ax 
     jmp CODE_SEG:boot2
 
 
@@ -49,12 +67,21 @@ gdt_pointer:
     dw  gdt_end - gdt_start - 1 
     dd  gdt_start
 
+disk:
+    db 0x0 
+
 CODE_SEG equ gdt_code_segment - gdt_start
 DATA_SEG equ gdt_data_segment - gdt_start 
 
+times 510 - ($-$$) db 0 
+dw 0xAA55 
+
+copy_target:
 
 ;Protected Mode 
 [bits 32]
+    
+    boot_msg: db "HELLO WORLD FROM BEYOND 512!!", 0 
 
 boot2:
     mov ax, DATA_SEG
@@ -71,19 +98,14 @@ boot2:
     lodsb 
     or al, al 
     jz halt
-    or eax, 0x0100 
+    or eax, 0x0F00 
     mov word [ebx], ax 
     add ebx, 2 
     jmp .loop 
+
 halt:
     cli 
     hlt 
-
-boot_msg: db "HELLO WORLD", 0 
-
-times 510 - ($-$$) db 0
-dw 0xaa55
-        
 
 
 
